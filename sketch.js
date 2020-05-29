e63b0157735e8d0edb8d78eae60cc12896f6dd43
kubo550 <43968748+kubo550@users.noreply.github.com>
2020-05-29 13:04:53 +0200

    v1.01

--- sketch.js
+++ sketch.js
@@ -1,18 +1,15 @@
 let fireworks = []
-let gravity, lightGravity
+let gravity
 function setup() {
   createCanvas(windowWidth, windowHeight)
-  gravity = createVector(0, 0.1)
+  gravity = createVector(0, 0.15)
 }
 setInterval(() => {
   fireworks.push(new Firework())
-
 }, 1000);
+
 function draw() {
   background(31)
-
-
-
   fireworks.forEach(f => f.update())
   fireworks = fireworks.filter(f => !f.isDead())
 }
